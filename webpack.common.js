1f82853fb623e91b741f266d753e08d7f7f6546a
akashag55 <2017pietcsakash005@poornima.org>
2021-12-01 17:54:52 +0530

    webpack update

---
+++ webpack.common.js
@@ -0,0 +1,68 @@
+const CommunicationIdentityClient = require("@azure/communication-administration").CommunicationIdentityClient
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin= require('mini-css-extract-plugin');
+const config = require("./config.json");
+
+if(!config || !config.connectionString || config.connectionString.indexOf('endpoint=') === -1)
+{
+    throw new Error("Update `config.json` with connection string");
+}
+
+const communicationIdentityClient = new  CommunicationIdentityClient(config.connectionString);
+ module.exports = {
+   entry: {
+     app: './src/index.js',
+   },
+   module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: (resourcePath, context) => {
+                console.log(resourcePath)
+                return path.relative(path.dirname(resourcePath), context) + '/'
+              },
+            },
+          },
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader',
+      }
+    ],
+  },
+   plugins: [
+     new HtmlWebpackPlugin({
+       title: 'Production',
+     }),
+     new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      })
+   ],
+   devServer: {
+    open: true,
+    before: function(app) {
+        app.post('/tokens/provisionUser', async (req, res) => {
+            try {
+                let communicationUserId = await communicationIdentityClient.createUser();
+                const tokenResponse = await communicationIdentityClient.issueToken(communicationUserId, ["voip"]);
+                res.json(tokenResponse);
+            } catch (error) {
+                console.error(error);
+            }
+        });
+    }
+   }
+ };
